dc48f5c7a03ce3e054071069b25eda6dcff4b797
siddharth849 <56408624+siddharth849@users.noreply.github.com>
2020-09-28 13:46:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants={
+    openWeatherMap:{
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"9fbfefcf59b7904fcfe439b24627e319"
+    }
+}
+
+module.exports= constants
---
+++ utils/weatherData.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const constants = require('../config');
+
+const WeatherData=(address,callback)=>{
+    const url=constants.openWeatherMap.BASE_URL+encodeURIComponent(address)+"&appid="+constants.openWeatherMap.SECRET_KEY
+    request({url,json:true},(error,{body})=>{
+        if(error){
+            callback("Can't fetch data from open weather map api",undefined)
+
+        }else{
+            callback(undefined,{
+                temperature:body.main.temp,
+                description:body.weather[0].description,
+                cityName:body.name
+            
+            })
+
+        }
+    })
+}
+
+module.exports=WeatherData
